860168e4f910207785c4ba539f57f33644d4f26a
Ben Young <developing.ben@gmail.com>
2025-06-20 14:31:31 -0500

    loops, arrays, slices

--- main.go
+++ main.go
@@ -5,49 +5,75 @@ import (
 )
 
 func main() {
-// Variables
+	age := 30
+	if age >= 18 {
+		fmt.Println("You are an adult")
+	} else if age >= 13 {
+		fmt.Println("You are a teenager")
+	} else {
+		fmt.Println("You are a child")
+	}
 
-	var name string  = "Ben"
-	fmt.Printf("Hello, %s!\n", name)
+	day := "Tuesday"
+	switch day {
+	case "Monday":
+		fmt.Println("Start of the week")
+	case "Tuesday", "Wednesday", "Thursday":
+		fmt.Println("Midweek")
+	case "Friday":
+		fmt.Println("TGIF")
+	default: 
+		fmt.Println("Weekend") 
+	}
 
-	age := 38
-	fmt.Printf("You are %d years old.\n", age)
+	for i := 0; i < 5; i++ {
+		fmt.Println("Iteration:", i)
+	}
 
-	var city string
-	city = "Columbia"
-	fmt.Printf("You live in %s.\n", city)
+	// while loop in Go is just a for loop with a condition
+	counter := 0
+	for counter < 5 {
+		fmt.Println("Counter:", counter)
+		counter++
+	}
 
-	// Zero values
-	var defaultInt int
-	var defaultFloat float64
-	var defaultString string
-	var defaultBool bool
-	fmt.Printf("Default int: %d, float: %f, string: '%s', bool: %t\n", defaultInt, defaultFloat, defaultString, defaultBool) 
-
-	var (
-		isEmployed bool = true
-		salary int = 75000
-		position string = "Software Engineer"
-	)
-	fmt.Printf("Employed: %t, Salary: $%d, Position: %s\n", isEmployed, salary, position)
-
-	const typedAge int = 38
-	const untypedAge = 38 
-	fmt.Println(typedAge == untypedAge) // true, same value but different types
+	// infinite loop if needed
 	
-	result := add(3, 4)
-	fmt.Println("This is result", result)
+	iterations := 0
+	for  {
+		if iterations >3 {
+			break
+		}
+		iterations++
+	}
 
-	sum, product := calculateSumAndProduct(10, 10)
-	fmt.Printf("Sum: %d, Product: %d\n", sum, product)
+	// Arrays and slices
+	// Arrays are fixed size, slices are dynamic
+	// [5]int is an array of 5 integers
+	number := [5]int{10,20,30,40,50}
+	// arrays cannot be changed after declaration
+	fmt.Printf("this is our array: %v\n", number)
+	fmt.Println("this is our last value", number[len(number)-2])
+
+	matrix := [2][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+	}
+
+	fmt.Printf("this is our matrix: %v\n", matrix)
+
+	// allNumbers := number[:];
+	// firstThree := number[0:3]
+
+	fruits := []string{"apple", "banana", "cherry"}
+	fmt.Printf("this is our slice of fruits: %v\n", fruits)
+
+	fruits = append(fruits, "strawberry")
+	fmt.Printf("after appending, our slice of fruits is: %v\n", fruits)
+
+	for index, value := range fruits {
+		fmt.Printf("Index: %d, Value: %s\n", index, value)
+	}
 }
 
-// if you have a return type, the function must return a value of that type
-// if you don't have a return type, the function can return nothing
-func add(a int, b int) int {
-	return a + b
-}
 
-func calculateSumAndProduct(a int, b int) (int, int) {
-	return a + b, a * b
-}
